203f03d31c95afa8050cf782e93e15e72227463a
Kris1027 <obarzanek.work@gmail.com>
2023-09-18 01:35:53 +0200

    add filter to selected region

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,7 @@ import Country from './Country';
 function Main() {
   const [countryData, setCountryData] = useState(null);
   const [filter, setFilter] = useState('');
+  const [selectedRegion, setSelectedRegion] = useState('all');
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/all`)
@@ -20,6 +21,10 @@ function Main() {
     setFilter(e.target.value);
   }
 
+  function handleRegionChange(e) {
+    setSelectedRegion(e.target.value);
+  }
+
   return (
     <main className='min-h-screen bg-sky-950 flex flex-col items-center'>
       <form className='flex flex-col gap-y-6 pt-6 pb-24 w-96'>
@@ -30,21 +35,26 @@ function Main() {
           value={filter}
           onChange={handleFilterChange}
         />
-        <select className=' max-w bg-sky-900 text-sky-100 py-4 px-6 rounded-md'>
-          <option value='default' disabled>
-            Filter by Region
-          </option>
-          <option>Africa</option>
-          <option>America</option>
-          <option>Asia</option>
-          <option>Europe</option>
-          <option>Oceania</option>
+        <select
+          className='max-w bg-sky-900 text-sky-100 py-4 px-6 rounded-md'
+          value={selectedRegion}
+          onChange={handleRegionChange}
+        >
+          <option value='all'>Filter by Region</option>
+          <option value='Africa'>Africa</option>
+          <option value='Americas'>Americas</option>
+          <option value='Asia'>Asia</option>
+          <option value='Europe'>Europe</option>
+          <option value='Oceania'>Oceania</option>
         </select>
       </form>
       {countryData !== null &&
         countryData
           .filter((country) =>
             country.name.common.toLowerCase().includes(filter.toLowerCase())
+          )
+          .filter((country) =>
+            selectedRegion === 'all' ? true : country.region === selectedRegion
           )
           .map((country) => (
             <Country key={country.name.common} countryData={country} />
