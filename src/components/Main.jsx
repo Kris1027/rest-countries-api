6462197f283118f25ce4f7309340eee5d49185e6
Kris1027 <obarzanek.work@gmail.com>
2023-09-17 18:56:33 +0200

    add url of api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+
+const API_URL = `https://restcountries.com/v3.1/name/poland`;
+
 function Main() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((resp) => resp.json())
+      .then((apiData) => {
+        setData(apiData);
+      });
+  }, []);
+
   return (
     <main className='min-h-screen bg-sky-950'>
       <form className='flex flex-col gap-y-6 py-6 px-6'>
@@ -8,7 +22,7 @@ function Main() {
           placeholder='Search for a country...'
         />
         <select className=' max-w bg-sky-900 text-sky-100 py-4 px-6'>
-          <option selected disabled>
+          <option value='default' disabled>
             Filter by Region
           </option>
           <option>Africa</option>
