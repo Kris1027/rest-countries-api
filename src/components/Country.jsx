acf1f1710aca7f28b018066507418dd346158af4
Kris1027 <obarzanek.work@gmail.com>
2023-09-18 01:11:00 +0200

    country component styled and rendered by map

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,23 @@
-function Country() {
-  return <div></div>;
+function Country({ countryData }) {
+  if (!countryData) return <div>Loading...</div>;
+
+  return (
+    <div className='bg-sky-900 text-sky-100 flex flex-col w-96 m-6 rounded-md overflow-hidden'>
+      <img src={countryData.flags.png} alt='flag' />
+      <div className='flex flex-col p-10'>
+        <h2 className='font-bold text-xl pb-4'>{countryData.name.common}</h2>
+        <p>
+          <strong>Population:</strong> {countryData.population}
+        </p>
+        <p>
+          <strong>Region:</strong> {countryData.region}
+        </p>
+        <p>
+          <strong>Capital:</strong> {countryData.capital}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Country;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,29 @@
 import { useEffect, useState } from 'react';
-
-const API_URL = `https://restcountries.com/v3.1/name/poland`;
+import Country from './Country';
 
 function Main() {
-  const [data, setData] = useState(null);
+  const [countryData, setCountryData] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((resp) => resp.json())
-      .then((apiData) => {
-        setData(apiData);
+    fetch(`https://restcountries.com/v3.1/all`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryData(data);
+      })
+      .catch((error) => {
+        console.error('Somethin went wrong grr:', error);
       });
   }, []);
 
   return (
-    <main className='min-h-screen bg-sky-950'>
-      <form className='flex flex-col gap-y-6 py-6 px-6'>
+    <main className='min-h-screen bg-sky-950 flex flex-col items-center'>
+      <form className='flex flex-col gap-y-6 pt-6 pb-24 w-96'>
         <input
           className='bg-sky-900 w-full text-sky-100 placeholder:text-sky-100 py-4 px-6 rounded-md'
           type='search'
           placeholder='Search for a country...'
         />
-        <select className=' max-w bg-sky-900 text-sky-100 py-4 px-6'>
+        <select className=' max-w bg-sky-900 text-sky-100 py-4 px-6 rounded-md'>
           <option value='default' disabled>
             Filter by Region
           </option>
@@ -32,6 +34,10 @@ function Main() {
           <option>Oceania</option>
         </select>
       </form>
+      {countryData !== null &&
+        countryData.map((country) => (
+          <Country key={country.name} countryData={country} />
+        ))}
     </main>
   );
 }
