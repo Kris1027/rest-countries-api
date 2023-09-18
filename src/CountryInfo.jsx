e61d3796c0f76b2f51a874a1d93be663466f8af3
Kris1027 <obarzanek.work@gmail.com>
2023-09-18 19:29:08 +0200

    CountryInfo styled

--- src/CountryInfo.jsx
+++ src/CountryInfo.jsx
@@ -18,40 +18,58 @@ function CountryInfo() {
           Back
         </button>
       </Link>
-      <div>
+      <div className='flex pt-12 flex-col lg:flex-row flex-wrap'>
         <div>
-          <img src={countryData[0].flags.png} alt='' />
+          <img className='pr-10' src={countryData[0].flags.png} alt='' />
         </div>
         <div>
-          <h2>{countryData[0].name.common}</h2>
-          <p>Native Name: {countryData[0].name.official}</p>
-          <p>Population: {formatPopulation(countryData[0].population)}</p>
-          <p>Region: {countryData[0].region}</p>
-          <p>Sub Region: {countryData[0].subregion}</p>
-          <p>Capital: {countryData[0].capital}</p>
+          <h2 className='font-bold text-xl pb-4'>
+            {countryData[0].name.common}
+          </h2>
           <p>
-            Currencies:
+            <strong>Native Name:</strong> {countryData[0].name.official}
+          </p>
+          <p>
+            <strong>Population:</strong>{' '}
+            {formatPopulation(countryData[0].population)}
+          </p>
+          <p>
+            <strong>Region:</strong> {countryData[0].region}
+          </p>
+          <p>
+            <strong>Sub Region:</strong> {countryData[0].subregion}
+          </p>
+          <p>
+            <strong>Capital:</strong> {countryData[0].capital}
+          </p>
+          <p>
+            <strong>Currencies:</strong>
             {Object.keys(countryData[0].currencies).map((currencyCode) => (
-              <span key={currencyCode}>
-                {countryData[0].currencies[currencyCode].name} (
-                {countryData[0].currencies[currencyCode].symbol} )
+              <span className='mx-2' key={currencyCode}>
+                {countryData[0].currencies[currencyCode].name}:{' '}
+                {countryData[0].currencies[currencyCode].symbol},
               </span>
             ))}
           </p>
           <p>
-            Languages:
+            <strong>Languages:</strong>
             {Object.keys(countryData[0].languages).map((languageCode) => (
-              <span key={languageCode}>
+              <span className='' key={languageCode}>
                 {countryData[0].languages[languageCode]}
               </span>
             ))}
           </p>
         </div>
         <div>
-          <p>
-            Border Countries:
+          <p className='pt-10'>
+            <strong>Border Countries:</strong>
             {Object.keys(countryData[0].borders).map((borderCode) => (
-              <span key={borderCode}>{countryData[0].borders[borderCode]}</span>
+              <span
+                className='bg-sky-900 gap-x-2 w-8 mx-4 py-2 px-4 rounded-lg'
+                key={borderCode}
+              >
+                {countryData[0].borders[borderCode]}
+              </span>
             ))}
           </p>
         </div>
