7ccf494483d282c6dd90f6ea80ced97fed5fda6f
Kris1027 <obarzanek.work@gmail.com>
2023-09-18 15:02:51 +0200

    Link on logo and on each country

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useTheme } from '../contexts/themeContext';
 
 function Country({ countryData }) {
@@ -6,25 +7,27 @@ function Country({ countryData }) {
   if (!countryData) return <div>Loading...</div>;
 
   return (
-    <div
-      className={`${
-        isDarkMode ? 'bg-sky-900 text-sky-100' : 'bg-sky-100 text-sky-900'
-      } flex flex-col w-96 m-6 rounded-md overflow-hidden duration-200 ease-linear`}
-    >
-      <img src={countryData.flags.png} alt='flag' />
-      <div className='flex flex-col p-10'>
-        <h2 className='font-bold text-xl pb-4'>{countryData.name.common}</h2>
-        <p>
-          <strong>Population:</strong> {countryData.population}
-        </p>
-        <p>
-          <strong>Region:</strong> {countryData.region}
-        </p>
-        <p>
-          <strong>Capital:</strong> {countryData.capital}
-        </p>
+    <Link to='/country'>
+      <div
+        className={`${
+          isDarkMode ? 'bg-sky-900 text-sky-100' : 'bg-sky-100 text-sky-900'
+        } flex flex-col w-96 m-6 rounded-md overflow-hidden duration-100 ease-linear cursor-pointer active:scale-90 hover:scale-110`}
+      >
+        <img src={countryData.flags.png} alt='flag' />
+        <div className='flex flex-col p-10'>
+          <h2 className='font-bold text-xl pb-4'>{countryData.name.common}</h2>
+          <p>
+            <strong>Population:</strong> {countryData.population}
+          </p>
+          <p>
+            <strong>Region:</strong> {countryData.region}
+          </p>
+          <p>
+            <strong>Capital:</strong> {countryData.capital}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useTheme } from '../contexts/themeContext';
 
 function Header() {
@@ -9,7 +10,9 @@ function Header() {
         isDarkMode ? 'bg-sky-900 text-sky-100' : 'bg-sky-100 text-sky-900'
       }  px-4 py-10 transition-colors duration-200 ease-linear`}
     >
-      <h1 className='font-bold'>Where in the world?</h1>
+      <Link to='/'>
+        <h1 className='font-bold'>Where in the world?</h1>
+      </Link>
       <p
         className='font-semibold cursor-pointer active:scale-90 hover:scale-110'
         onClick={toggleDarkMode}
